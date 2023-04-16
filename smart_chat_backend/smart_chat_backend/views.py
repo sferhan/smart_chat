9b2b768a87f5215936414dd31cf9e02e70109fab
Farhan Haider <>
2023-04-16 02:36:13 -0700

    migrate db locally

--- smart_chat_backend/smart_chat_backend/views.py
+++ smart_chat_backend/smart_chat_backend/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 
+from users.models import ChatUser
+
 
 def hello(request):
+    a = ChatUser.objects.all().count()
+    print(f"\n\n\na\n\n\n")
     return HttpResponse("Hello World")
