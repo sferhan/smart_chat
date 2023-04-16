f66a61c49665244939817a4c023831d2b483a623
yasi98 <ypakdel@ucdavis.edu>
2023-04-16 15:56:47 -0700

    frontend UI

--- smart_chat_frontend/src/App.js
+++ smart_chat_frontend/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+/** 
+Front end chat UI code from https://getstream.io/chat/react-chat/tutorial/ 
+**/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+
+import 'stream-chat-react/dist/css/v2/index.css';
+import './layout.css';
+import { useClient } from './useClient';
+
+const chatClient = new StreamChat('dz5f4d5kzrue');
+const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZGFtcC1tdWQtMSIsImV4cCI6MTY4MTQyNzU4OH0.lOinRvJKZq4nPiNLKYI2NCQ4vQCN0DOt1QOH8Oht9SY';
+
+chatClient.connectUser(
+  {
+    id: 'damp-mud-1',
+    name: 'damp',
+    image: 'https://getstream.io/random_png/?id=damp-mud-1&name=damp',
+  },
+  userToken,
+);
+
+const channel = chatClient.channel('messaging', 'custom_channel_id', {
+  // add as many custom fields as you'd like
+  image: 'https://www.drupal.org/files/project-images/react.png',
+  name: 'Talk about React',
+  members: ['damp-mud-1'],
+});
+
+const App = () => (
+  <Chat client={chatClient} theme='str-chat__theme-dark'>
+    <Channel channel={channel}>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
 
 export default App;
