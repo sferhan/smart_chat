89b2be18f1810a5be35407b1d7684a098f1a90c7
yasi98 <ypakdel@ucdavis.edu>
2023-04-16 15:58:36 -0700

    frontend UI

--- smart_chat_frontend/src/App.js
+++ smart_chat_frontend/src/App.js
@@ -8,7 +8,7 @@ import { Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window
 
 import 'stream-chat-react/dist/css/v2/index.css';
 import './layout.css';
-import { useClient } from './useClient';
+
 
 const chatClient = new StreamChat('dz5f4d5kzrue');
 const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZGFtcC1tdWQtMSIsImV4cCI6MTY4MTQyNzU4OH0.lOinRvJKZq4nPiNLKYI2NCQ4vQCN0DOt1QOH8Oht9SY';
